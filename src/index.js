6c10177f7234c70cd04fddc02f976bd87aa1552a
mchanphilly <56936452+mchanphilly@users.noreply.github.com>
2022-05-06 14:24:32 -0400

    Initial boilerplate

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ class FlipCard extends HTMLElement {
         let side = this.getAttribute("side");
         this.#side = (side === null) ? 0: side;
 
+        this.children[1 - this.#side].setAttribute("hidden", "");
         console.log(`Constructed card with side ${this.#side}`);
     }
 
@@ -13,10 +14,39 @@ class FlipCard extends HTMLElement {
         return this.#side;
     }
 
-    flip() {
-        this.#side = 1 - this.#side;
-        console.log(`Flipped to ${this.#side}`);
+    set side(s) {
+        this.#side = s;
+        this.setAttribute("side", s);
     }
+
+    static get observedAttributes() {
+        return ["side"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "side") this.#side = newValue;
+    }
+
+    flip() {
+        let back = this.#side;
+        let front = this.#side = 1 - this.#side;
+        console.log(`Flipped to ${this.#side}`);
+
+        let children = this.children;
+
+        this.setAttribute("side", front);
+        children[back].setAttribute("hidden", "");
+        children[front].removeAttribute("hidden");
+    }
+
+
+    // #getFront() {
+    //     return this.children[0];
+    // }
+
+    // #getBack() {
+    //     return this.children[1];
+    // }
 
 
 }
